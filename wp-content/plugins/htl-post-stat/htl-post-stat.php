b0013f3454348e0feefcfb124041e4f478808eff
David Kaufmann <david@kaufmann.dev>
2023-06-02 10:36:03 +0100

    fix: update wp-content/plugins/htl-post-stat/htl-post-stat.php

--- wp-content/plugins/htl-post-stat/htl-post-stat.php
+++ wp-content/plugins/htl-post-stat/htl-post-stat.php
@@ -28,15 +28,43 @@ class PostStat{
     }
 
     function pluginSettingMenuEntry(){
-        add_options_page('Post Stat Settings' , 'Post Stat', 'manage_options', 'htl-post-stat', array($this, 'pluginSettingHTML'));
+        add_options_page(
+            'Post Stat Settings' ,
+            'Post Stat',
+            'manage_options',
+            'htl-post-stat',
+            array($this, 'pluginSettingHTML')
+        );
     }
     
     /**
      * Markup für die Umsetzung der Backend Page des HTL Post Stat Plugins
      */
-    
+
     function pluginSettingHTML(){
-        echo "<p>Hello</p>";
+        ?>
+            <div class="wrap">
+                <h1>Post Stat Settings</h1>
+                <form action="options.php" method="post">
+                    <?php
+                        settings_fields('post-stat-plugin');
+                        do_settings_sections('post-stat-settings-page');
+                        submit_button();
+                    ?>
+                </form>
+        <?php
+    }
+
+    /**
+     * name: muss sich mit der ID des jeweiligen Setting FIelds decken => "Display location" ist das psp_location
+     */
+
+    function locationHTML(){
+        ?>
+            <select name="psp_location">
+                <option value="0">Beginning of post</option>
+                <option value="1">End of post</option>
+        <?php
     }
 }
 
